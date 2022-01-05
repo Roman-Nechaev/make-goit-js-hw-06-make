fcac03acb15ea51b82a439f28a97223b64d69d4e
Roman-Nechaev <nrchaevroman@gmail.com>
2022-01-06 01:02:03 +0200

    1233

--- js/task-01.js
+++ js/task-01.js
@@ -6,9 +6,20 @@
 // console.log(navEl)
 
 
-const allEl = document.querySelector('.item-Animals')
+const allEl = document.querySelector('.item-animals')
 console.log(allEl)
 console.log("Category:", allEl.textContent)
+
+const animalsEl  = document.getElementsByClassName("el1")[0].childElementCount;
+console.log('Elements:', animalsEl);
+
+const allEl1 = document.querySelector('.item-products')
+console.log(allEl1)
+console.log("Category:", allEl1.textContent)
+
+const allEl2 = document.querySelector('.itam-technologies')
+console.log(allEl2)
+console.log("Category:", allEl2.textContent)
 
 // Number of categories: 3
 
