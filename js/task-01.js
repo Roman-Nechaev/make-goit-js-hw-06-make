10100b3d55e329b48dfe9f939702f096c9ea9f42
Roman-Nechaev <nrchaevroman@gmail.com>
2022-01-05 21:04:34 +0200

    32

---
+++ js/task-01.js
@@ -0,0 +1,22 @@
+// const allEl = document.querySelector('#categories')
+// console.log(allEl)
+
+
+// const navEl = allEl.querySelectorAll('ul')
+// console.log(navEl)
+
+
+const allEl = document.querySelector('.item-Animals')
+console.log(allEl)
+console.log("Category:", allEl.textContent)
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
