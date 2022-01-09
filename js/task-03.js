8160b2c65a69439e04d146116bb960b2389825c7
Roman-Nechaev <nrchaevroman@gmail.com>
2022-01-10 01:51:49 +0200

    task-3-done

--- js/task-03.js
+++ js/task-03.js
@@ -1,50 +1,53 @@
-const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-];
+  const images = [
+    {
+      url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'White and Black Long Fur Cat',
+    },
+    {
+      url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+      url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Group of Horses Running',
+    },
+  ];
 
-// Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
+  // Напиши скрипт для создания галереи изображений по массиву данных. В HTML есть список ul.gallery.
 
-// <ul class="gallery"></ul>
-// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+  // <ul class="gallery"></ul>
+  // Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
 
-// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
-// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+  // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+  // Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
 
 
 
-const addGalleryEl = document.querySelector('.gallery');
-console.log(addGalleryEl);
+  const addGalleryEl = document.querySelector('.gallery');
+  // console.log(addGalleryEl);
 
-const element = images.map(option => {
+  const makeGalleryEl = (images) => {
+    return images.map((image) => {
+    const addEl = document.createElement('li');
+    const itemEl = document.createElement('img');
+    addEl.append(itemEl);
+    itemEl.src = image.url;
+    itemEl.alt = image.alt;
+    itemEl.width = 360;
+    itemEl.height = 360;
   
-  const itemEl = document.createElement('img');
-  itemEl.src = option.url;
-  itemEl.alt = option.alt;
-  itemEl.width = 360;
-  console.log(itemEl);
- 
- return itemEl;
-});
+
+    console.log(itemEl);
+  
+  return addEl;
+  
+    });
+  };
 
 
-addGalleryEl.append(...element);
+  const galleryEl = makeGalleryEl(images);
+const testImg = addGalleryEl.append(...galleryEl)
 
-
-
-
-const imageEl = document.createElement('li');
-console.log(imageEl);
 
 
 
