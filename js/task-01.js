6de514f92b789dfa8e04d6af2bd2b29adb93e41b
Roman-Nechaev <nrchaevroman@gmail.com>
2022-01-09 02:19:56 +0200

    task-2-done

--- js/task-01.js
+++ js/task-01.js
@@ -2,16 +2,17 @@
 // console.log(navigatorEl)
 
 const navigatorEl = document.querySelector('#categories');
-console.log(navigatorEl);
+// console.log(navigatorEl);
+
 
 const firstamountEl = navigatorEl.querySelectorAll('h2');
 console.log('Number of categories:', firstamountEl.length); // Number of categories: 3
+
 
 const firstNameEl = navigatorEl.querySelector('h2');
 console.log('Category:', firstNameEl.textContent); // Category: Animals
 
 const firstquantityEl = navigatorEl.querySelectorAll('.item ul');
-
 console.log('Elements:', firstquantityEl[0].childElementCount); // Elements: 4
 
 
@@ -20,15 +21,14 @@ const secondNameEl = navigatorEl.querySelectorAll('h2');
 console.log('Category:', secondNameEl[1].textContent); // Category: Products
 
 const secondquantityEl = navigatorEl.querySelectorAll('.item ul');
-
 console.log('Elements:', secondquantityEl[1].childElementCount); // Elements: 3
+
 
 
 const lastNameEl = navigatorEl.querySelectorAll('h2');
 console.log('Category:', lastNameEl[2].textContent); // Category: Technologies
 
 const lastquantityEl = navigatorEl.querySelectorAll('.item ul');
-
 console.log('Elements:', lastquantityEl[2].childElementCount); // Elementlast
 
 
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+// 1.Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// 2.Добавит название ингредиента как его текстовое содержимое.
+// 3.Добавит элементу класс item.
+// 4.После чего вставит все <li> за одну операцию в список ul.ingredients.
+
+  const ingredientsListEl = document.querySelector('#ingredients');
+
+ const addElement = ingredients.map(option => {
+    const addItemEl = document.createElement('li');
+    addItemEl.classList.add('item');
+    addItemEl.textContent = option;
+  
+    return addItemEl;
+  });
+  console.log(addElement);
+
+
+
+  ingredientsListEl.append(...addElement);
+
+
+
+
+
+
+
+
